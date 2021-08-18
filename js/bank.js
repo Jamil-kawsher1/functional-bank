8d086c52d83741e4b020aadced2d2c0855a153e2
Jamil Kawsher <jamil15-9734@diu.edu.bd>
2021-08-18 20:57:01 +0600

    first commit

--- js/bank.js
+++ js/bank.js
@@ -1,14 +1,31 @@
-var depositG = 0;
+var balance = 0;
 
-//Deposit Section
-//add event listener and display given value to deposit section
-const depositBtn = document.getElementById("addMoneyBtn").addEventListener("click", function () {
+function getInputValue() {
     //geting all info of dposit input field on variable inputValueDesposit
     const inputValueDeposit = document.getElementById("addMoney");
     //geting user inputed value from deposit input field and converted it to float
     const inputValueDepositFloat = parseFloat(inputValueDeposit.value);
-    //holding the user inputed value for deposit to use it in balance section
-    depositG = inputValueDepositFloat;
+    //clearing input value filed of deposit ammount
+    inputValueDeposit.value = "";
+    return inputValueDepositFloat;
+
+
+
+
+}
+
+
+
+
+
+//Deposit Section
+//add event listener and display given value to deposit section
+const depositBtn = document.getElementById("addMoneyBtn").addEventListener("click", function () {
+
+
+    const inputValueDepositFloat = getInputValue();
+
+
     //holding and seting display value of deposit
     const dipositDisplay = document.getElementById("depositField");
 
@@ -19,7 +36,7 @@ const depositBtn = document.getElementById("addMoneyBtn").addEventListener("clic
     const finalDeposit = inputValueDepositFloat + iniDepositFloat;
 
     dipositDisplay.innerText = finalDeposit.toFixed(2);
-    inputValueDeposit.value = "";
+
 
     // balance part
     //holdin and getting info of balaceDasboard filed in initialBalance variable
@@ -29,9 +46,38 @@ const depositBtn = document.getElementById("addMoneyBtn").addEventListener("clic
     //linking up balace with deposit
     const totalBalance = initialBalanceFloat + inputValueDepositFloat;
     initialBalance.innerText = totalBalance.toFixed(2);
-
+    balance = totalBalance;
 
 })
+
+//widthdrawal part
+const moneyOutBtn = document.getElementById("moneyOutBtn").addEventListener("click", function () {
+
+    //setting display and update widthdrwal dashboard
+    //geting input value from user 
+    const moneyOut = document.getElementById("moneyOut");
+    const userInputedvalue = moneyOut.value;
+    moneyOut.value = "";
+    //conver to float
+    const userInputedvalueFloat = parseFloat(userInputedvalue);
+    //geting currnet balance of widthdrwal dashboard
+    const widthdrawField = document.getElementById("widthdrawField");
+    const widthdrawalFiledInnervalue = parseFloat(widthdrawField.innerText);
+    const finalWidthdrawal = userInputedvalueFloat + widthdrawalFiledInnervalue;
+    widthdrawField.innerText = finalWidthdrawal;
+
+    //getting inner value of blance filed from display
+    const balaceDashboard = document.getElementById("balaceDashboard");
+    const balaceDasboardInner = balaceDashboard.innerText;
+    const balaceDasboardInnerFloat = parseFloat(balaceDasboardInner);
+    const finalDashboardBoardBalance = balaceDasboardInnerFloat - userInputedvalueFloat;
+    balaceDashboard.innerText = finalDashboardBoardBalance.toFixed(2);
+
+})
+
+
+
+
 
 
 
