274dae8388b9b7eae200cf51ddb68366f545cd35
Jamil Kawsher <jamil15-9734@diu.edu.bd>
2021-08-19 12:18:35 +0600

    bug fixex NAN input Problem Solved and withdrwal overflow widthdrawal problem fixed

--- js/bank.js
+++ js/bank.js
@@ -1,6 +1,7 @@
 var balance = 0;
 
 function getInputValue(value) {
+
     //geting all info of dposit input field on variable inputValueDesposit
     const inputValueDeposit = document.getElementById(value);
     //geting user inputed value from deposit input field and converted it to float
@@ -24,13 +25,22 @@ function updateDepoWidth(fieldname, ammount) {
 
 }
 
-function balanceUpdate(inputdepo, isTrue) {
-
+function currentBalance() {
     const initialBalance = document.getElementById("balaceDashboard");
     //converting the Initial value to float 
     const initialBalanceFloat = parseFloat(initialBalance.innerText);
-    //linking up balace with deposit
+    return initialBalanceFloat;
 
+
+}
+
+
+function balanceUpdate(inputdepo, isTrue) {
+    const initialBalance = document.getElementById("balaceDashboard");
+
+    //linking up balace with deposit
+    const initialBalanceFloat = currentBalance();
+    console.log(initialBalanceFloat);
     if (isTrue == true) {
         const totalBalance = initialBalanceFloat + inputdepo;
         initialBalance.innerText = totalBalance.toFixed(2);
@@ -54,9 +64,12 @@ function balanceUpdate(inputdepo, isTrue) {
 const depositBtn = document.getElementById("addMoneyBtn").addEventListener("click", function () {
 
     const inputdepo = getInputValue("addMoney");
+    if (inputdepo > 0) {
+        updateDepoWidth("depositField", inputdepo);
+        balanceUpdate(inputdepo, true);
 
+    }
 
-    balanceUpdate(inputdepo, true);
 
 
 })
@@ -68,9 +81,16 @@ const moneyOutBtn = document.getElementById("moneyOutBtn").addEventListener("cli
     //conver to float
     const userInputedvalueFloat = getInputValue("moneyOut")
     //geting currnet balance of widthdrwal dashboard
-    updateDepoWidth("widthdrawField", userInputedvalueFloat);
+    const currentDashboardbalance = currentBalance();
+    if (userInputedvalueFloat > 0 && currentDashboardbalance > userInputedvalueFloat) {
+        updateDepoWidth("widthdrawField", userInputedvalueFloat);
 
-    balanceUpdate(userInputedvalueFloat, false);
+        balanceUpdate(userInputedvalueFloat, false);
+
+    }
+    if (currentDashboardbalance < userInputedvalueFloat) {
+        alert("Please Inser a lower value!!Input Vale must be less then Current value!!!");
+    }
 
 
 
